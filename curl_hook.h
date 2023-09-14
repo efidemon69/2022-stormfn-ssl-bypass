e0c70561f953857d654f5d23eb6734208ea45b6b
ring0bp <93293947+ring0bp@users.noreply.github.com>
2023-09-14 19:44:16 +0200

    Update curl_hook.h

--- curl_hook.h
+++ curl_hook.h
@@ -16,25 +16,24 @@ auto curl_mh_hook( CURL* cURL, uintptr_t option, va_list data ) -> CURLcode
 {
 	std::vector<std::regex> urls =
 	{
-        std::regex( "/content/api/pages/fortnite-game" ),
-        std::regex( "/fortnite/api/game/v2/profile" ),
-        std::regex( "/fortnite/api/cloudstorage/system" ),
-        std::regex( "/fortnite/api/storefront/v2/keychain" ),
-        std::regex( "/versioncheck" ),
-        std::regex( "/fortnite/api/game/v2/br-inventory/account" ),
-        std::regex( "/affiliate/api" ),
-        std::regex( "/discord/api" ),
-        std::regex( "/waitingroom/api" ),
-        std::regex( "/socialban/api" ),
-        std::regex( "/fortnite/api/game/v2/matchmakingservice" ),
-        std::regex( "/fortnite/api/matchmaking/session" ),
+	      std::regex( "/content/api/pages/fortnite-game" ),
+              std::regex( "/fortnite/api/game/v2/profile" ),
+              std::regex( "/fortnite/api/cloudstorage/system" ),
+              std::regex( "/fortnite/api/storefront/v2/keychain" ),
+              std::regex( "/versioncheck" ),
+              std::regex( "/fortnite/api/game/v2/br-inventory/account" ),
+              std::regex( "/affiliate/api" ),
+              std::regex( "/discord/api" ),
+              std::regex( "/waitingroom/api" ),
+              std::regex( "/socialban/api" ),
+              std::regex( "/fortnite/api/game/v2/matchmakingservice" ),
+              std::regex( "/fortnite/api/matchmaking/session" ),
 	};
 
 	if (0x2712)
 	{
 		std::regex  epic( "(.*).ol.epicgames.com" );
         std::string our_backend = "http://" + std::string( BACKEND_HOST ) + ":" + BACKEND_PORT;
-
 		std::string data_swap = data;
 
         for ( const auto& pattern : urls ) 
@@ -46,7 +45,7 @@ auto curl_mh_hook( CURL* cURL, uintptr_t option, va_list data ) -> CURLcode
                 break;
             }
         }
-	}
+    }
 
     return _curl_easy_setopt( cURL, option, data);
 }
